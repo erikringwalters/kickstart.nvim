15af5e20cb9d527c74042f71fc9939363b9f1018
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-08-10 21:06:53 -0700

    rust stuff

--- init.lua
+++ init.lua
@@ -431,14 +431,14 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[ ] Search Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch in existing [B]uffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -884,20 +884,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     ---@diagnostic disable-next-line: missing-fields
-  --     require('tokyonight').setup {
-  --       styles = {
-  --         comments = { italic = false }, -- Disable italics in comments
-  --       },
-  --     }
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     -- vim.cmd.colorscheme 'tokyonight-night' -- example
-  --   end,
+    --   'folke/tokyonight.nvim',
+    --   priority = 1000, -- Make sure to load this before all the other start plugins.
+    --   config = function()
+    --     ---@diagnostic disable-next-line: missing-fields
+    --     require('tokyonight').setup {
+    --       styles = {
+    --         comments = { italic = false }, -- Disable italics in comments
+    --       },
+    --     }
+    --     -- Load the colorscheme here.
+    --     -- Like many other themes, this one has different styles, and you could load
+    --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --     -- vim.cmd.colorscheme 'tokyonight-night' -- example
+    --   end,
   },
 
   -- Highlight todo, notes, etc in comments
@@ -979,8 +979,9 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require 'config.mappings',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,8 @@
 -- See the kickstart.nvim README for more information
 return {
   -- ***Erik's plugins*** --
+
+  -- Melange theme with custom colors
   {
     'savq/melange-nvim',
 
@@ -11,15 +13,80 @@ return {
     config = function()
       vim.cmd 'colorscheme melange'
       vim.api.nvim_set_hl(0, 'Normal', { bg = '#161111' })
-      vim.api.nvim_set_hl(0, 'String', { fg = '#886499' })
+      vim.api.nvim_set_hl(0, 'String', { fg = '#9682af' })
     end,
   },
 
-  -- Easy Commenting
+  -- Easy Commenting (use commands gc or gcc)
   {
     'numToStr/Comment.nvim',
     opts = {
       -- add any options here
     },
+  },
+
+  -- Rust configs pulled from dreamsofcode's setup video
+  {
+    'rust-lang/rust.vim',
+    ft = 'rust',
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    'saecki/crates.nvim',
+    ft = { 'toml' },
+    config = function(_, opts)
+      local crates = require 'crates'
+      crates.setup(opts)
+      require('cmp').setup.buffer {
+        sources = { { name = 'crates' } },
+      }
+      crates.show()
+      require('core.utils').load_mappings 'crates'
+    end,
+  },
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    lazy = false,
+    config = function(_, opts)
+      require 'nvim-dap-virtual-text'
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap',
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    config = function()
+      require('dapui').setup()
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap, dapui = require 'dap', require 'dapui'
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    config = function()
+      require('dapui').setup()
+    end,
   },
 }
